35f18236a3a2c9499422c5799a831aff3e7c1d00
CJ Cowden <cjcow439@learner.dvusd.org>
2022-11-30 09:33:49 -0700

    Saved

--- ROLLL-a-BALLL/Assets/Scripts/PlayerController.cs
+++ ROLLL-a-BALLL/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rb = GetComponet<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
+    }
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movemnetValue.Get<Vector2>();
+
     }
 }
